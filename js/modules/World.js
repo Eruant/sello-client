c9ccb096dd83a836f6cd4b2bce7050dc516f754a
Eruant <info@littleball.co.uk>
2013-06-20 23:22:31 +0100

    Added bynd logo

--- js/modules/World.js
+++ js/modules/World.js
@@ -32,7 +32,47 @@ define(['Class'], function (Class) {
         },
 
         draw: function (ctx) {
-            ctx.fillRect(0, 0, this.options.width, this.options.height);
+
+            ctx.save();
+            ctx.translate(0.5, 0.5);
+
+            ctx.fillStyle = '#f06';
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(200, 0);
+            ctx.lineTo(0, 200);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.fillStyle = '#fff';
+            ctx.save();
+            ctx.translate(20, 20);
+
+            ctx.beginPath();
+            
+            ctx.moveTo(0, 0);
+            ctx.lineTo(20, 20);
+            ctx.lineTo(20, 40);
+            ctx.lineTo(0, 20);
+            ctx.lineTo(0, 0);
+
+            ctx.moveTo(0, 22);
+            ctx.lineTo(19, 41);
+            ctx.lineTo(0, 60);
+            ctx.lineTo(0, 22);
+
+            ctx.moveTo(20, 42);
+            ctx.lineTo(40, 42);
+            ctx.lineTo(60, 62);
+            ctx.lineTo(0, 62);
+            ctx.lineTo(20, 42);
+
+            ctx.closePath();
+            
+            ctx.fill();
+            ctx.restore();
+
+            ctx.restore();
         }
 
     });
