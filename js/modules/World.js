8896fed8e1cd1cd984e3579313614412c7502239
Matt Gale <matt@littleball.co.uk>
2013-06-21 17:49:22 +0100

    Added Bynd logo to the world

--- js/modules/World.js
+++ js/modules/World.js
@@ -25,22 +25,25 @@ define(['Class'], function (Class) {
             }
 
             this.options = opts;
+			
+			this.canvas = document.createElement('canvas');
+            this.canvas.width = 210;
+            this.canvas.height = 210;
+			this.ctx = this.canvas.getContext('2d');
+			
+			this.drawBynd(this.ctx);
 
         },
-
-        update: function (stepTime, progress) {
-        },
-
-        draw: function (ctx) {
-
-            ctx.save();
+		
+		drawBynd: function (ctx) {
+			ctx.save();
             ctx.translate(0.5, 0.5);
 
             ctx.fillStyle = '#f06';
             ctx.beginPath();
             ctx.moveTo(0, 0);
-            ctx.lineTo(200, 0);
-            ctx.lineTo(0, 200);
+            ctx.lineTo(210, 0);
+            ctx.lineTo(0, 210);
             ctx.closePath();
             ctx.fill();
 
@@ -54,18 +57,41 @@ define(['Class'], function (Class) {
             ctx.lineTo(20, 20);
             ctx.lineTo(20, 40);
             ctx.lineTo(0, 20);
-            ctx.lineTo(0, 0);
 
             ctx.moveTo(0, 22);
             ctx.lineTo(19, 41);
             ctx.lineTo(0, 60);
-            ctx.lineTo(0, 22);
 
             ctx.moveTo(20, 42);
             ctx.lineTo(40, 42);
             ctx.lineTo(60, 62);
             ctx.lineTo(0, 62);
-            ctx.lineTo(20, 42);
+			
+			ctx.moveTo(60, 63);
+			ctx.lineTo(60, 83);
+			ctx.lineTo(40, 83);
+			ctx.lineTo(40, 63);
+			
+			ctx.moveTo(60, 84);
+			ctx.lineTo(40, 104);
+			ctx.lineTo(40, 84);
+			
+			ctx.moveTo(39, 84);
+			ctx.lineTo(39, 104);
+			ctx.lineTo(20, 84);
+			
+			ctx.moveTo(0, 65);
+			ctx.lineTo(37, 104);
+			ctx.lineTo(14, 104);
+			ctx.lineTo(0, 89);
+			
+			ctx.moveTo(1, 64);
+			ctx.lineTo(20, 64);
+			ctx.lineTo(10, 73);
+			
+			ctx.moveTo(20, 66);
+			ctx.lineTo(20, 84);
+			ctx.lineTo(11, 74);
 
             ctx.closePath();
             
@@ -73,6 +99,13 @@ define(['Class'], function (Class) {
             ctx.restore();
 
             ctx.restore();
+		},
+
+        update: function (stepTime, progress) {
+        },
+
+        draw: function (ctx) {
+            ctx.drawImage(this.canvas, 0,0);
         }
 
     });
